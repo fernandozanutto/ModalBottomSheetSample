2ad691f64aec07b79701d3006dacdc12db3d9e5f
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-08-11 14:41:13 -0300

    Initial commit

---
+++ app/src/main/java/com/fzanutto/modalbottomsheetsample/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.fzanutto.modalbottomsheetsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MyBottomSheet().show(supportFragmentManager, MyBottomSheet.TAG)
+    }
+}
+
+
+class MyBottomSheet: BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.modal_bottom_sheet_content, container, false)
+
+    companion object {
+        const val TAG = "ModalBottomSheet"
+    }
+}
